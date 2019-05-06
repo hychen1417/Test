67b1c68f9a6218acd81f04d89f79ca07fec1eb32
chenhuayu <chenhuayu@meituan.com>
2019-05-06 16:20:35 +0800

    测试View与Activity生命周期关系

--- app/src/main/java/com/example/chenhuayu/test/MyViewActivity.java
+++ app/src/main/java/com/example/chenhuayu/test/MyViewActivity.java
@@ -2,16 +2,42 @@ package com.example.chenhuayu.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 public class MyViewActivity extends Activity {
     private ScrollView scrollView;
     private LinearLayout linearLayout;
+    private final String TAG = "MyViewActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_view);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
 }
--- app/src/main/java/com/example/chenhuayu/test/view/MyCardView.java
+++ app/src/main/java/com/example/chenhuayu/test/view/MyCardView.java
@@ -6,6 +6,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.LinearLayout;
 
 /**
@@ -13,6 +15,8 @@ import android.widget.LinearLayout;
  */
 
 public class MyCardView extends LinearLayout {
+    
+    private final String TAG = "MyViewActivity";
 
     private int radius = 8;
     private int gap = 8;
@@ -33,8 +37,62 @@ public class MyCardView extends LinearLayout {
         init();
     }
 
-    public void init(){
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        Log.d(TAG, "onFinishInflate: ");
+    }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        Log.d(TAG, "onMeasure: ");
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        Log.d(TAG, "onLayout: ");
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Log.d(TAG, "onSizeChanged: ");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.d(TAG, "onKeyDown: ");
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        Log.d(TAG, "onKeyUp: ");
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        Log.d(TAG, "onWindowFocusChanged: ");
+        super.onWindowFocusChanged(hasWindowFocus);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        Log.d(TAG, "onAttachedToWindow: ");
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        Log.d(TAG, "onDetachedFromWindow: ");
+        super.onDetachedFromWindow();
+    }
+
+    public void init(){
+        Log.d(TAG, "init: ");
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setColor(Color.WHITE);
         mPaint.setDither(true);
@@ -43,6 +101,7 @@ public class MyCardView extends LinearLayout {
 
     @Override
     protected void onDraw(Canvas canvas){
+        Log.d(TAG, "onDraw: ");
         super.onDraw(canvas);
         int circleNumber = getWidth()/(2*(gap + radius));
         for (int i = 1;i <= circleNumber + 1;i++){
